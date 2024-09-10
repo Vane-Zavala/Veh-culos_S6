09f0edad5671378e16906f1586bbbcf054eab198
Vane-Zavala <vanzavala12@gmail.com>
2024-09-09 23:12:57 -0600

    Primer commit

--- app.py
+++ app.py
@@ -1,33 +1,33 @@
-import streamlit as st
 import pandas as pd
 import plotly.express as px
+import streamlit as st
+pd.set_option('display.max_columns', None)
 
-# Leer el conjunto de datos
-vehiculos = pd.read_csv('vehicles_us.csv')
+car_data = pd.read_csv('C:/Users/vanes/Downloads/proyect_6_trip/proyect_6_trip/vehicles_us.csv')
 
-# Agregar un encabezado
-st.header('Análisis Interactivo de Anuncios de Coches')
+#Agregar un encabezado
+st.header('Análisis Interactivo de anuncios de Coches')
 
-# Crear botones para histogramas y gráficos de dispersión
+#Crear botones para histogramas y gráficos de dispersión
 boton_hist_1 = st.button('Construir histograma')  # Crear un botón para el histograma
 boton_disp_1 = st.button('Construir gráfico de dispersión')  # Crear un botón para el gráfico de dispersión
 
-# Acción al hacer clic en el botón para el histograma
+#Acción al hacer clic en el botón para el histograma
 if boton_hist_1:
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-    
-    # Crear un histograma (ajustar la columna según tus datos)
-    fig_hist = px.histogram(vehiculos, x='odometer')  # Reemplaza 'odometer' con la columna correcta si es necesario
-    
-    # Mostrar el gráfico Plotly interactivo
+
+#Crear un histograma (ajustar la columna según tus datos)
+    fig_hist = px.histogram(car_data, x='odometer')  # Reemplaza 'odometer' con la columna correcta si es necesario
+
+#Mostrar el gráfico Plotly interactivo
     st.plotly_chart(fig_hist, use_container_width=True)
 
-# Acción al hacer clic en el botón para el gráfico de dispersión
+#Acción al hacer clic en el botón para el gráfico de dispersión
 if boton_disp_1:
     st.write('Creación de un diagrama de dispersión para el conjunto de datos de anuncios de venta de coches')
-    
-    # Crear un gráfico de dispersión (ajustar columnas según tus datos)
-    fig_scatter = px.scatter(vehiculos, x='odometer', y='price')  # Reemplaza 'odometer' y 'price' con las columnas correctas
-    
-    # Mostrar el gráfico Plotly interactivo
+
+#Crear un gráfico de dispersión (ajustar columnas según tus datos)
+    fig_scatter = px.scatter(car_data, x='odometer', y='price')  # Reemplaza 'odometer' y 'price' con las columnas correctas
+
+#Mostrar el gráfico Plotly interactivo
     st.plotly_chart(fig_scatter, use_container_width=True)
